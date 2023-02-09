a0d8d919e077c148ae9cb3f60a7ffce71c49384f
Deon Donald <2644603D@student.gla.ac.uk>
2023-02-09 17:27:14 +0000

    webpage displays hey there partner

--- rango/urls.py
+++ rango/urls.py
@@ -19,9 +19,9 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from rango import views
+from django.urls import path
+from rango import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('rango/', include('rango.urls')),
-    path('admin/', admin.site.urls),
 ]
